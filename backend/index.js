1b2b1155cac888e5e2e2dc2982df848c5943438c
Azhar Ali <149366630+Devazharruk@users.noreply.github.com>
2024-10-02 20:09:49 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  app.use(cors({
+  origin: 'http://localhost:3000',  
+  credentials: true,               
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+  
+}));
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
