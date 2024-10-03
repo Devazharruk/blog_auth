54678a0cdc0d04a7c00d690f31c4ce264b54917f
Azhar Ali <149366630+Devazharruk@users.noreply.github.com>
2024-10-03 08:08:29 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,22 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
-app.options("*", cors()); // Enable preflight for all routes
-
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://blog-auth-liart.vercel.app",
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.use(express.json());
