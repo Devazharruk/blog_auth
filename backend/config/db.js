08a2466ccf9f9d7806c76470f3f30b76ee8e6ba6
Azhar Ali <149366630+Devazharruk@users.noreply.github.com>
2024-10-03 15:06:36 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://junaid:Junaid12321@blog-shard-00-00.ox8fd.mongodb.net:27017,blog-shard-00-01.ox8fd.mongodb.net:27017,blog-shard-00-02.ox8fd.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-wy6pxh-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Blog");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
